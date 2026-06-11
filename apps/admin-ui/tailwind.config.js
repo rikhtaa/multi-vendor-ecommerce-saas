29ca9e5b4ecae6511a0f7124d47d2ac98295657a
Rekhta Menahil <rekhta674@gmail.com>
2026-06-11 14:12:11 +0500

    add admin ui

---
+++ apps/admin-ui/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './{src,pages,components,app}/**/*.{ts,tsx,js,jsx,html}',
+    '!./{src,pages,components,app}/**/*.{stories,spec}.{ts,tsx,js,jsx,html}',
+    '../admin-ui/src/**/*.{js,ts,tsx,jsx}',
+    "../../packages/components/**/*.{js,ts,tsx,jsx}",
+//     ...createGlobPatternsForDependencies(__dirname)
+  ],
+   theme: {
+    extend: {
+      fontFamily: {
+        Poppins:["var(--font-poppins)"],
+      }
+    },
+  },
+  plugins: [],
+};
