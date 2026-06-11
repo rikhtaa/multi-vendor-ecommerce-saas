95040b5dddbb2a946057604f8942707ec9d45fc3
Rekhta Menahil <rekhta674@gmail.com>
2026-06-11 14:20:56 +0500

    add isAdmin middleware

--- packages/middleware/isAuthenticated.ts
+++ packages/middleware/isAuthenticated.ts
@@ -13,30 +13,33 @@ const isAuthenticated = async (req: any, res: Response, next: NextFunction) => {
             return res.status(401).json({message: "Unauthorized Token missing."})
         }
         
-        //verify token
         const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!) as {
             id: string
-            role: "user" | "seller"
+            role: "user" | "seller" | "admin"  
         }
         
         if(!decoded){
             return res.status(401).json({message: "Unauthorized Invalid Token"})
         }
         
-
         let account;
 
         if(decoded.role === "user"){
-          account = await prisma.users.findUnique({
-            where: {id: decoded.id}
-          })
-          req.user = account
-        }else if(decoded.role === "seller"){
-          account = await prisma.sellers.findUnique({
-            where: {id: decoded.id},
-            include: {shop: true}
-          })
-          req.seller = account
+            account = await prisma.users.findUnique({
+                where: {id: decoded.id}
+            })
+            req.user = account
+        } else if(decoded.role === "seller"){
+            account = await prisma.sellers.findUnique({
+                where: {id: decoded.id},
+                include: {shop: true}
+            })
+            req.seller = account
+        } else if(decoded.role === "admin"){  
+            account = await prisma.users.findUnique({
+                where: {id: decoded.id}
+            })
+            req.user = account
         }
         
         if(!account){
@@ -44,7 +47,6 @@ const isAuthenticated = async (req: any, res: Response, next: NextFunction) => {
         }
 
         req.role = decoded.role
-
         return next()
     } catch (error) {
         return res
